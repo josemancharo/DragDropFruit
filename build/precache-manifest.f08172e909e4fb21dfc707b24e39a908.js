79ea142c01bd8a7aae52b4687a11758adc708293
Isaiah Pettingill <josemancharo@gmail.com>
2021-02-06 02:11:30 +0000

    let build transfer over

---
+++ build/precache-manifest.f08172e909e4fb21dfc707b24e39a908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd4d1ae933498da65f88d6c564d87a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce7945482b0794e9ced",
+    "url": "/static/css/main.d351a5b3.chunk.css"
+  },
+  {
+    "revision": "eff6acdd8f1ade1d868a",
+    "url": "/static/js/2.3342b93d.chunk.js"
+  },
+  {
+    "revision": "3ce7945482b0794e9ced",
+    "url": "/static/js/main.c9c20776.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
