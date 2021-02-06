8cd2e3557d5fabeec0939f22cc510462c989e1e5
Isaiah Pettingill <josemancharo@gmail.com>
2021-02-06 03:17:38 +0000

    added build again

---
+++ build/precache-manifest.c7f3b52861cf0369891776fbd30c16b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fd5aa4c0a071594b705fb0f5a7c80b",
+    "url": "/DragDropFruit/index.html"
+  },
+  {
+    "revision": "99bd94706db3949f35a2",
+    "url": "/DragDropFruit/static/css/main.d351a5b3.chunk.css"
+  },
+  {
+    "revision": "eff6acdd8f1ade1d868a",
+    "url": "/DragDropFruit/static/js/2.3342b93d.chunk.js"
+  },
+  {
+    "revision": "99bd94706db3949f35a2",
+    "url": "/DragDropFruit/static/js/main.fb2e000e.chunk.js"
+  },
+  {
+    "revision": "d4184f562686f18a9c75",
+    "url": "/DragDropFruit/static/js/runtime-main.f782781a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DragDropFruit/static/media/logo.5d5d9eef.svg"
+  }
+]);
